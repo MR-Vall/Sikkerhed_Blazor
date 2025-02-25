1850d0e47999c7e9da35372c98822b9ed97c3c6f
MR-Vall <markrobbers3@gmail.com>
2025-02-25 12:05:53 +0100

    Add project files.

---
+++ BlazorAppTest/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using BlazorApp1.Components.Pages;
+using Bunit;
+using Bunit.TestDoubles;
+using Xunit;
+
+namespace BlazorAppTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Home_ShowsNotAuthenticatedMessage_WhenUserIsNotAuthenticated()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var authContext = ctx.AddTestAuthorization();
+            authContext.SetNotAuthorized();
+
+            // Act
+            var cut = ctx.RenderComponent<Home>();
+
+            // Assert
+            cut.MarkupMatches("<h1>Hello, world!</h1>\r\nWelcome to your new app.\r\n<p>You are not an admin.</p>\r\n<p>You are not authenticated.</p>");
+        }
+
+        [Fact]
+        public void Home_ShowsAuthenticatedMessage_WhenUserIsAuthenticated()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var authContext = ctx.AddTestAuthorization();
+            authContext.SetAuthorized("testuser");
+
+            // Act
+            var cut = ctx.RenderComponent<Home>();
+
+            // Assert
+            cut.MarkupMatches("<h1>Hello, world!</h1>\r\nWelcome to your new app.\r\n<p>You are not an admin.</p>\r\n<p>You are authenticated.</p>");
+        }
+
+        [Fact]
+        public void Home_ShowsAdminMessage_WhenUserIsAdmin()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var authContext = ctx.AddTestAuthorization();
+            authContext.SetAuthorized("adminuser");
+            authContext.SetRoles("Admin");
+
+            // Act
+            var cut = ctx.RenderComponent<Home>();
+
+            // Assert
+            cut.MarkupMatches("<h1>Hello, world!</h1>\r\nWelcome to your new app.\r\n<p>You are admin!</p>\r\n<p>You are authenticated.</p>");
+        }
+
+        [Fact]
+        public void Home_ShowsNotAdminMessage_WhenUserIsNotAdmin()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var authContext = ctx.AddTestAuthorization();
+            authContext.SetAuthorized("testuser");
+
+            // Act
+            var cut = ctx.RenderComponent<Home>();
+
+            // Assert
+            cut.MarkupMatches("<h1>Hello, world!</h1>\r\nWelcome to your new app.\r\n<p>You are not an admin.</p>\r\n<p>You are authenticated.</p>");
+        }
+    }
+}
+
